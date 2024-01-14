90b715786614918454c63282f7f6b1c4555d8a2f
Heather van der Dys <heather.vanderdys@gmail.com>
2024-01-14 13:25:44 -0600

    Call to action button - component

---
+++ src/components/CTA.jsx
@@ -0,0 +1,11 @@
+export default function CTA() {
+  return (
+    <a
+      href="mailto:team@vanderdys.design"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <button>Contact Me</button>
+    </a>
+  );
+}
