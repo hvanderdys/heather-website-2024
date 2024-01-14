1921db5232b774ae7f3c46906496d28d0c6f5973
Heather van der Dys <heather.vanderdys@gmail.com>
2024-01-14 14:17:31 -0600

    resume

--- src/pages/index.js
+++ src/pages/index.js
@@ -615,7 +615,7 @@ export default function Home() {
                 <Image
                   src="/2023/2023_NTTC_Conference-001.png"
                   alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -625,7 +625,7 @@ export default function Home() {
                 <Image
                   src="/2023/2023_NTTC_Conference-002.png"
                   alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -635,7 +635,7 @@ export default function Home() {
                 <Image
                   src="/2023/2023_NTTC_Conference-003.png"
                   alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -645,7 +645,7 @@ export default function Home() {
                 <Image
                   src="/2023/2023_NTTC_Conference-004.png"
                   alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -655,7 +655,7 @@ export default function Home() {
                 <Image
                   src="/2023/2023_NTTC_Conference-005.png"
                   alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -665,7 +665,7 @@ export default function Home() {
                 <Image
                   src="/2023/2023_NTTC_Conference-006.png"
                   alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -675,7 +675,7 @@ export default function Home() {
                 <Image
                   src="/2023/2023_NTTC_Conference-007.png"
                   alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -685,7 +685,7 @@ export default function Home() {
                 <Image
                   src="/2023/2023_NTTC_Conference-008.png"
                   alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -695,7 +695,7 @@ export default function Home() {
                 <Image
                   src="/2023/2023_NTTC_Conference-009.png"
                   alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -705,7 +705,7 @@ export default function Home() {
                 <Image
                   src="/2023/2023_NTTC_Conference-010.png"
                   alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -715,7 +715,7 @@ export default function Home() {
                 <Image
                   src="/2023/2023_NTTC_Conference-011.png"
                   alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -725,7 +725,7 @@ export default function Home() {
                 <Image
                   src="/2023/2023_NTTC_Conference-012.png"
                   alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -735,7 +735,7 @@ export default function Home() {
                 <Image
                   src="/2023/2023_NTTC_Conference-013.png"
                   alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -751,7 +751,7 @@ export default function Home() {
                   <Image
                     src="/2023/2023_CharlieAward_Eliminating-Blind-Spot.jpeg"
                     alt="Vercel Logo"
-                    className={styles.vercelLogo}
+                    className={styles.scribe}
                     width={300}
                     height={225}
                     priority
@@ -762,7 +762,7 @@ export default function Home() {
                 <Image
                   src="/2023/2023_CharlieAward_High-Performance-Teambuilding.jpeg"
                   alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -774,8 +774,8 @@ export default function Home() {
               <div className={styles.grid}>
                 <Image
                   src="/2022/2022_NTTC_Conference_001.png"
-                  alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  alt="Session illustration - Digital Recording/Live Scribing Example"
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -784,8 +784,8 @@ export default function Home() {
               <div className={styles.grid}>
                 <Image
                   src="/2022/2022_NTTC_Conference_002.png"
-                  alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  alt="Session illustration - Digital Recording/Live Scribing Example"
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -794,8 +794,8 @@ export default function Home() {
               <div className={styles.grid}>
                 <Image
                   src="/2022/2022_NTTC_Conference_003.png"
-                  alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  alt="Session illustration - Digital Recording/Live Scribing Example"
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -804,8 +804,8 @@ export default function Home() {
               <div className={styles.grid}>
                 <Image
                   src="/2022/2022_NTTC_Conference_004.png"
-                  alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  alt="Session illustration - Digital Recording/Live Scribing Example"
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -814,8 +814,8 @@ export default function Home() {
               <div className={styles.grid}>
                 <Image
                   src="/2022/2022_NTTC_Conference_005.png"
-                  alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  alt="Session illustration - Digital Recording/Live Scribing Example"
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -824,8 +824,8 @@ export default function Home() {
               <div className={styles.grid}>
                 <Image
                   src="/2022/2022_NTTC_Conference_006.png"
-                  alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  alt="Session illustration - Digital Recording/Live Scribing Example"
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -834,8 +834,8 @@ export default function Home() {
               <div className={styles.grid}>
                 <Image
                   src="/2022/2022_NTTC_Conference_007.png"
-                  alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  alt="Session illustration - Digital Recording/Live Scribing Example"
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -844,8 +844,8 @@ export default function Home() {
               <div className={styles.grid}>
                 <Image
                   src="/2022/2022_NTTC_Conference_008.png"
-                  alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  alt="Session illustration - Digital Recording/Live Scribing Example"
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -854,8 +854,8 @@ export default function Home() {
               <div className={styles.grid}>
                 <Image
                   src="/2022/2022_NTTC_Conference_009.png"
-                  alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  alt="Session illustration - Digital Recording/Live Scribing Example"
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -864,8 +864,8 @@ export default function Home() {
               <div className={styles.grid}>
                 <Image
                   src="/2022/2022_NTTC_Conference_010.png"
-                  alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  alt="Session illustration - Digital Recording/Live Scribing Example"
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -874,8 +874,8 @@ export default function Home() {
               <div className={styles.grid}>
                 <Image
                   src="/2022/2022_NTTC_Conference_011.png"
-                  alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  alt="Session illustration - Digital Recording/Live Scribing Example"
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -884,8 +884,8 @@ export default function Home() {
               <div className={styles.grid}>
                 <Image
                   src="/2022/2022_NTTC_Conference_012.png"
-                  alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  alt="Session illustration - Digital Recording/Live Scribing Example"
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   prioritys
@@ -894,8 +894,8 @@ export default function Home() {
               <div className={styles.grid}>
                 <Image
                   src="/2022/2022_NTTC_Conference_013.png"
-                  alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  alt="Session illustration - Digital Recording/Live Scribing Example"
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -904,8 +904,8 @@ export default function Home() {
               <div className={styles.grid}>
                 <Image
                   src="/2022/2022_NTTC_Conference_014.png"
-                  alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  alt="Session illustration - Digital Recording/Live Scribing Example"
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -914,8 +914,8 @@ export default function Home() {
               <div className={styles.grid}>
                 <Image
                   src="/2022/2022_NTTC_Conference_015.png"
-                  alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  alt="Session illustration - Digital Recording/Live Scribing Example"
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -924,8 +924,8 @@ export default function Home() {
               <div className={styles.grid}>
                 <Image
                   src="/2022/2022_NTTC_Conference_016.png"
-                  alt="Vercel Logo"
-                  className={styles.vercelLogo}
+                  alt="Session illustration - Digital Recording/Live Scribing Example"
+                  className={styles.scribe}
                   width={300}
                   height={225}
                   priority
@@ -933,6 +933,106 @@ export default function Home() {
               </div>
             </div>
           </div>
+        </article>
+        <article className={styles.resume}>
+          <h2>My Career and Story</h2>
+          <h3>How this Crazy “Talent Stack” Found Me</h3>
+          <p>
+            When I say “Talent Stack,” I am quoting from others. It is simply
+            how we “Combine a bunch of <strong>skills</strong> [and experiences]
+            in a way that makes [all of us unique and] extraordinary.” I entered
+            the professional arena armed with a distinctive amalgamation of
+            skills and experiences—a talent stack that has evolved over the
+            years. I came into the work world with{" "}
+            <strong>
+              art fundamentals, business strategy education, printing
+              experience,
+            </strong>{" "}
+            and <strong>lighting knowledge.</strong>
+            This was “stacked” on top of my driven{" "}
+            <strong>work ethic, multi-tasking,</strong>and{" "}
+            <strong>sales experience</strong> from working my way through
+            college the previous 4.5-5 years. I also started with the skills or
+            talents of <strong>organizing, 3D visualization,</strong> and{" "}
+            <strong>quick learning/adaptation.</strong> I graduated high school
+            and college with almost perfect grades, had remained 1st-2nd chair
+            in the band throughout college, and had never applied for a job I
+            didn’t receive. Needless to say, I was barely into my 20s, and as I
+            learned lessons and came into the working world, I realized many
+            things about the work space and carreer/business development.
+          </p>
+          <br />
+          <p>
+            My first job in my BFA field was as an apprentice/assistant under a
+            small business owner, and maybe that is why, still to this day, I
+            love <strong>working B2B</strong> (business to business.)
+          </p>{" "}
+          <br />
+          <p>
+            As I transitioned to my role as a Creative and Communication
+            Director for a church, I honed my skills in{" "}
+            <strong>
+              digital engagement, branding consistency, efficient design,
+              project management, timeline planning, team leading, business
+              budgeting,
+            </strong>{" "}
+            and <strong>paid marketing</strong>
+            (digital and print.)
+          </p>{" "}
+          <br />
+          <p>
+            Then, in 2016, my family gathered our love for{" "}
+            <strong>
+              music, art, children, small businesses, local community,
+            </strong>{" "}
+            and <strong>adventure.</strong> We moved to a remote village in
+            Romania and started our own consulting company where we could have
+            the flexibility to help run church camps, tutor children, and much
+            more. Running this consulting firm through ups and downs taught me
+            about{" "}
+            <strong>
+              client relations, product design, wireframing, UI/UX,
+              consulting/contracting, brand management, brand guidelines, social
+              media marketing, copywriting, app development, website management,
+              content management, data illustration, packaging design, menu
+              design, responsive design,
+            </strong>{" "}
+            and <strong>work-life balance.</strong> Also during these 6 years
+            overseas, I learned basic <strong>front-end development</strong>{" "}
+            from my souse as we juggled multiple projects at the same time.
+            Working with non-profits also added to my talent stack by{" "}
+            <strong>
+              planning events, marketing events, leading worship, photographing
+              events, multilingual web management, filming content, editing and
+              translating video,
+            </strong>{" "}
+            and <strong>translating content.</strong> The time traveling and the
+            time in entrepreneurship also allowed me to teach, which is the best
+            way to learn and reinforce knowledge. I taught intro classes to
+            <strong>
+              Excel, Microsoft, HTML, digital security, web design, branding,
+              crafts, painting, watercolor,
+            </strong>{" "}
+            and <strong>budgeting.</strong>
+          </p>
+          <br />
+          <p>
+            As we transitioned back to the United States (Still traveling back
+            and forth as time allows) I helped co-found a <strong>LMS</strong>{" "}
+            and <strong>employee engagement</strong> software company and
+            started freelancing as a{" "}
+            <strong>
+              Digital Scribe (also known as a Digital Recorder or illustrative
+              Notetaker).
+            </strong>
+          </p>
+          <br />
+          <p>
+            I have since continued contracting and freelancing for multiple
+            clients. I design and develop for{" "}
+            <strong>user experiences, landing pages,</strong>and{" "}
+            <strong>application designs.</strong>
+          </p>
         </article>
         <Footer />
       </main>
