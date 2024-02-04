ffa668f7cfbfbe0ff16d1df170e7a08119165434
Heather van der Dys <heather.vanderdys@gmail.com>
2024-02-04 16:46:13 -0600

    Added social to carreer and made resume component

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import SocialIcons from "../components/SocialIcons";
 import Footer from "../components/Footer";
 import CTA from "../components/CTA";
 import Menu from "@/components/Menu";
+import Resume from "@/components/Resume";
 import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -962,106 +963,7 @@ export default function Home() {
             </div>
           </div>
         </article>
-        <article id="resume" className={styles.resume}>
-          <h2>My Career and Story</h2>
-          <h3>How This Crazy “Talent Stack” Found Me</h3>
-          <p>
-            When I say “Talent Stack,” I am quoting from others. It is simply
-            how we “Combine a bunch of <strong>skills</strong> [and experiences]
-            in a way that makes [all of us unique and] extraordinary.” I entered
-            the professional arena armed with a distinctive amalgamation of
-            skills and experiences—a talent stack that has evolved over the
-            years. I came into the work world with{" "}
-            <strong>
-              art fundamentals, business strategy education, printing
-              experience,
-            </strong>{" "}
-            and <strong>lighting knowledge.</strong> This was “stacked” on top
-            of my driven <strong>work ethic, multi-tasking,</strong> and{" "}
-            <strong>sales experience</strong> from working my way through
-            college the previous 4.5-5 years. I also started with the skills or
-            talents of <strong>organizing, 3D visualization,</strong> and{" "}
-            <strong>quick learning/adaptation.</strong> I graduated high school
-            and college with almost perfect grades, had remained 1st-2nd chair
-            in the band throughout college, and had never applied for a job I
-            didn’t receive. Needless to say, I was barely in my 20s, and as I
-            learned lessons and came into the working world, I realized many
-            things about the workspace and career/business development.
-          </p>
-          <br />
-          <p>
-            My first job in my BFA field was as an apprentice/assistant under a
-            small business owner, and maybe that is why, still to this day, I
-            love <strong>working B2B</strong> (business to business.)
-          </p>{" "}
-          <br />
-          <p>
-            As I transitioned to my role as a Creative and Communication
-            Director for a church, I honed my skills in{" "}
-            <strong>
-              digital engagement, branding consistency, efficient design,
-              project management, timeline planning, team leading, business
-              budgeting,
-            </strong>{" "}
-            and <strong>paid marketing</strong>
-            (digital and print.)
-          </p>{" "}
-          <br />
-          <p>
-            Then, in 2016, my family gathered our love for{" "}
-            <strong>
-              music, art, children, small businesses, local community,
-            </strong>{" "}
-            and <strong>adventure.</strong> We moved to a remote village in
-            Romania and started our own consulting company where we could have
-            the flexibility to help run church camps, tutor children, and much
-            more. Running this consulting firm through ups and downs taught me
-            about{" "}
-            <strong>
-              client relations, product design, wireframing, UI/UX,
-              consulting/contracting, brand management, brand guidelines, social
-              media marketing, copywriting, app development, website management,
-              content management, data illustration, packaging design, menu
-              design, responsive design,
-            </strong>{" "}
-            and <strong>work-life balance.</strong> Also during these 6 years
-            overseas, I learned basic <strong>front-end development</strong>{" "}
-            from my souse as we juggled multiple projects at the same time.
-            Working with non-profits also added to my talent stack by{" "}
-            <strong>
-              planning events, marketing events, leading worship, photographing
-              events, multilingual web management, filming content, editing and
-              translating video,
-            </strong>{" "}
-            and <strong>translating content.</strong> The time traveling and the
-            time in entrepreneurship also allowed me to teach, which is the best
-            way to learn and reinforce knowledge. I taught intro classes to
-            <strong>
-              {" "}
-              Excel, Microsoft, HTML, digital security, web design, branding,
-              crafts, painting, watercolor,
-            </strong>{" "}
-            and <strong>budgeting.</strong>
-          </p>
-          <br />
-          <p>
-            As we transitioned back to the United States (Still traveling back
-            and forth as time allows) I helped co-found an <strong>LMS</strong>{" "}
-            and <strong>employee engagement</strong> software company and
-            started freelancing as a{" "}
-            <strong>
-              Digital Scribe (also known as a Digital Recorder or illustrative
-              Notetaker).
-            </strong>
-          </p>
-          <br />
-          <p>
-            I have since continued contracting and freelancing for multiple
-            clients. I design and develop{" "}
-            <strong>user experiences, landing pages,</strong> and{" "}
-            <strong>application designs.</strong>
-          </p>
-        </article>
+        <Resume />
         <article id="consultation" className={styles.consultation}>
           <div className={styles.inner}>
             <h2>Design and Development Consultation Services</h2>
