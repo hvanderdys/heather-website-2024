724f5b2ea135daf83c512517ca63a02d83d1bf2c
Heather van der Dys <heather.vanderdys@gmail.com>
2024-02-08 16:22:28 -0600

    view more on tech portfolio

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,14 @@ import { ReactElement } from "react";
 import SocialIcons from "../components/SocialIcons";
 import Footer from "../components/Footer";
 import CTA from "../components/CTA";
+import TechPortfolio from "../components/TechPortfolio";
 import Menu from "@/components/Menu";
 import Resume from "@/components/Resume";
 import Link from "next/link";
 import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
+
 const ReadMore = () => {
   const [open, setOpen] = useState(true);
   const toggle = () => {
@@ -19,11 +21,13 @@ const ReadMore = () => {
   };
   return (
     <div>
-      <button type="button" onClick={toggle}>
-        Read <b>{open ? "More" : "Less"}</b>
-      </button>
       <div className={open ? styles.open : styles.closed}>
         <Resume />
+      </div>
+      <div className={styles.row}>
+        <button type="button" onClick={toggle}>
+          Read {open ? "More" : "Less"}
+        </button>
       </div>
     </div>
   );
@@ -280,314 +284,7 @@ export default function Home() {
               </section>
               <CTA />
             </header>
-            <div className={styles.portfolioGrid}>
-              <div className={styles.grid}>
-                <Link
-                  href="https://www.behance.net/gallery/185679559/Custom-Web-Design-in-Wordpress-with-Elemento-Pro"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className={styles.card}
-                >
-                  <Image
-                    src="/Tech/001-QE-wordpressSite.jpeg"
-                    alt="App/Web Portfolio item: WordPress Elementor Custom Website Design - Client Work Example"
-                    className={styles.vercelLogo}
-                    width={300}
-                    height={200}
-                    priority
-                  />
-                  <div>
-                    <h3>
-                      Client Website<span>-&gt;</span>
-                    </h3>
-                    <p>
-                      <em>Custom WordPress Theme | Elementor</em>
-                    </p>
-                    <p>Completed in 2022</p>
-                  </div>
-                </Link>
-              </div>
-              <div className={styles.grid}>
-                <Link
-                  href="https://www.behance.net/gallery/185673105/High-Fidelity-website-Prototype"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className={styles.card}
-                >
-                  <Image
-                    src="/Tech/002-MW-FigmaWireframe.jpeg"
-                    alt="App/Web Portfolio item: Figma High Fidelity Animated Wireframe - Client Work Example"
-                    className={styles.vercelLogo}
-                    width={300}
-                    height={200}
-                    priority
-                  />
-                  <div>
-                    <h3>
-                      High-Fidelity Client Wireframe<span>-&gt;</span>
-                    </h3>
-                    <p>
-                      <em>Created in Figma | Animated in Adobe XD</em>
-                    </p>
-                    <p>Completed in 2020</p>
-                  </div>
-                </Link>
-              </div>
-              <div className={styles.grid}>
-                <Link
-                  href="https://www.behance.net/gallery/130606335/Modern-web-design-illustration-and-video"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className={styles.card}
-                >
-                  <Image
-                    src="/Tech/003-PL-WebApplication.jpeg"
-                    alt="App/Web Portfolio item: Custom React JS Website Design - Client Work Example"
-                    className={styles.vercelLogo}
-                    width={300}
-                    height={200}
-                    priority
-                  />
-                  <div>
-                    <h3>
-                      Landing Page, Branding, & App<span>-&gt;</span>
-                    </h3>
-                    <p>
-                      <em>Javascript, React, CSS, and Adobe</em>
-                    </p>
-                    <p>Completed in 2021</p>
-                  </div>
-                </Link>
-              </div>
-              <div className={styles.grid}>
-                <Link
-                  href="https://www.behance.net/gallery/185756527/French-themed-Game-Illustrations"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className={styles.card}
-                >
-                  <Image
-                    src="/Tech/004-FG-GameDesign.jpeg"
-                    alt="App/Web Portfolio item: Game Illustration - Brakeys Game Jam"
-                    className={styles.vercelLogo}
-                    width={300}
-                    height={200}
-                    priority
-                  />
-                  <div>
-                    <h3>
-                      Game Illustration<span>-&gt;</span>
-                    </h3>
-                    <p>
-                      <em>Illustrated in Procreate | Brakeys Game Jam</em>
-                    </p>
-                    <p>Completed in 2022</p>
-                  </div>
-                </Link>
-              </div>
-              <div className={styles.grid}>
-                <Link
-                  href="https://www.behance.net/gallery/185761461/Actor-Website-and-Branding"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className={styles.card}
-                >
-                  <Image
-                    src="/Tech/005-GC-WixWebsite.jpeg"
-                    alt="App/Web Portfolio item: Custom Actor Film Wix Client Website - Client Work Example"
-                    className={styles.vercelLogo}
-                    width={300}
-                    height={200}
-                    priority
-                  />
-                  <div>
-                    <h3>
-                      Custom Wix Client Website<span>-&gt;</span>
-                    </h3>
-                    <p>Completed in 2023</p>
-                  </div>
-                </Link>
-              </div>
-              <div className={styles.grid}>
-                <Link
-                  href="https://www.behance.net/gallery/185755299/HTMLCSS-working-High-Fidelity-Mockup"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className={styles.card}
-                >
-                  <Image
-                    src="/Tech/006-DT-HTMLProject.jpeg"
-                    alt="HTML/CSS Client Product Search - Client Work Example"
-                    className={styles.vercelLogo}
-                    width={300}
-                    height={200}
-                    priority
-                  />
-                  <div>
-                    <h3>
-                      HTML/CSS Client Product Search<span>-&gt;</span>
-                    </h3>
-                    <p>Completed in 2022</p>
-                  </div>
-                </Link>
-              </div>
-              <div className={styles.grid}>
-                <Link
-                  href="https://www.behance.net/gallery/154704193/A-website-Revamp-Process-(2016-2022)"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className={styles.card}
-                >
-                  <Image
-                    src="/Tech/007-BM-JSWebsite.jpg"
-                    alt="Custom React Client Website - Client Work Example"
-                    className={styles.vercelLogo}
-                    width={300}
-                    height={200}
-                    priority
-                  />
-                  <div>
-                    <h3>
-                      Custom React Client Website<span>-&gt;</span>
-                    </h3>
-                    <p>
-                      <em>React App with Javascript and CSS</em>
-                    </p>
-                    <p>Completed in 2023</p>
-                  </div>
-                </Link>
-              </div>
-              <div className={styles.grid}>
-                <Link
-                  href="https://www.behance.net/gallery/125364613/Custom-Static-Web-Design"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className={styles.card}
-                >
-                  <Image
-                    src="/Tech/008-SF-ReactWebsite.png"
-                    alt="App/Custom Static 1-Page Client Website - Client Work Example"
-                    className={styles.vercelLogo}
-                    width={300}
-                    height={200}
-                    priority
-                  />
-                  <div>
-                    <h3>
-                      1-Page Client Website<span>-&gt;</span>
-                    </h3>
-                    <p>
-                      <em>React App with Javascript and CSS</em>
-                    </p>
-                    <p>Completed in 2022</p>
-                  </div>
-                </Link>
-              </div>
-              <div className={styles.grid}>
-                <Link
-                  href="https://www.behance.net/gallery/89332547/Personal-Business-Web-Design"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className={styles.card}
-                >
-                  <Image
-                    src="/Tech/009-Vd-ReactWebsite.png"
-                    alt="App/React Multipage | Previous Business - Personal Multipage Business Example"
-                    className={styles.vercelLogo}
-                    width={300}
-                    height={200}
-                    priority
-                  />
-                  <div>
-                    <h3>
-                      Multipage | Previous Business<span>-&gt;</span>
-                    </h3>
-                    <p>
-                      <em>React App with Javascript and CSS</em>
-                    </p>
-                    <p>Completed in 2021</p>
-                  </div>
-                </Link>
-              </div>
-              <div className={styles.grid}>
-                <Link
-                  href="https://www.behance.net/gallery/76978861/Full-Branding-Strategy-through-web-development"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className={styles.card}
-                >
-                  <Image
-                    src="/Tech/010-CS-ReactNativeMultiLingual.png"
-                    alt="App/Non-Profit React App Multi-Lingual - Non-Profit Example"
-                    className={styles.vercelLogo}
-                    width={300}
-                    height={200}
-                    priority
-                  />
-                  <div>
-                    <h3>
-                      Non-Profit & Multi-Lingual<span>-&gt;</span>
-                    </h3>
-                    <p>
-                      <em>React application in JavaScript and CSS</em>
-                    </p>
-                    <p>Completed in 2020</p>
-                  </div>
-                </Link>
-              </div>
-              <div className={styles.grid}>
-                <Link
-                  href="https://www.behance.net/gallery/185657915/Fireside-Book-Publishing-and-Landing-Page"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className={styles.card}
-                >
-                  <Image
-                    src="/Tech/011-FS-ReactWebsite.png"
-                    alt="App/React 1-page Client Landing Website - Client Work Example"
-                    className={styles.vercelLogo}
-                    width={300}
-                    height={200}
-                    priority
-                  />
-                  <div>
-                    <h3>
-                      1-page Client Landing Website<span>-&gt;</span>
-                    </h3>
-                    <p>
-                      <em>React application in JavaScript and CSS</em>
-                    </p>
-                    <p>Completed in 2020</p>
-                  </div>
-                </Link>
-              </div>
-              <div className={styles.grid}>
-                <Link
-                  href="https://www.behance.net/gallery/185653499/Countdowner-Desktop-Application"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className={styles.card}
-                >
-                  <Image
-                    src="/Tech/012-CD-ReactNativeDesktopApp.png"
-                    alt="App/Desktop Client Application - Client Work Example"
-                    className={styles.vercelLogo}
-                    width={300}
-                    height={200}
-                    priority
-                  />
-                  <div>
-                    <h3>
-                      Desktop Client Application<span>-&gt;</span>
-                    </h3>
-                    <p>
-                      <em>Collaboration Native App with JS and CSS</em>
-                    </p>
-                    <p>Completed in 2018</p>
-                  </div>
-                </Link>
-              </div>
-            </div>
+            <TechPortfolio />
           </div>
           <div className={styles.border}>
             <svg
