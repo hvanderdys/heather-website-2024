6bf162a28d39cfc82564bf3902830e2bcd1a70b8
Heather van der Dys <heather.vanderdys@gmail.com>
2024-02-15 11:00:29 -0600

    Add CTA to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import SocialIcons from "@/components/SocialIcons";
 import Menu from "@/components/Menu";
+import CTA from "@/components/CTA";
 
 export default function Footer() {
   return (
     <footer>
       <Menu />
+      <CTA />
       <SocialIcons />
     </footer>
   );
